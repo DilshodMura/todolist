f71ca81974e4b1590ca32b1bc34808d5cd4fc784
Dilshod Muratov <muratovdilshod2@gmail.com>
2023-02-24 11:08:19 +0500

    updates for gh pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HasRoutes>
+    <HashRouter>
     <App />
-    </HasRoutes>
+    </HashRouter>
   </React.StrictMode>
 );
